673505af9c46fc1d88995c13dbccdec41ea7e007
tienhai488 <97999852+tienhai488@users.noreply.github.com>
2023-04-02 11:13:42 +0700

    contact subcribe

---
+++ app/controllers/Contact.php
@@ -0,0 +1,88 @@
+<?php
+
+class Contact extends Controller
+{
+    public $__model, $__request, $__dataForm;
+    private $data = [];
+    public function __construct()
+    {
+        $this->__model = $this->model("admin/ContactsModel");
+        $this->__request = new Request();
+        
+    }
+
+    public function index(){
+        $this->data['title'] = "Liên hệ";
+        $this->data['content'] = 'blocks/contact';
+        $this->renderView('layouts/client_layout',$this->data);
+    }
+
+    public function subcribe(){
+        if (isPost()) {
+            $email = $_POST['email'];
+            $check = false;
+            $msg = "";
+
+            $dataInsert = [
+                'email'=>$email,
+                'status'=>1,
+                'note'=>'Chưa xử lý',
+                'create_at'=>date('Y-m-d H:i:s'),
+            ];
+
+            $status = $this->__model->addTableData('subcribes',$dataInsert);
+            if($status){
+                $msg = "Gửi email thành công!";
+            }else{
+                $msg = "Hệ thống lỗi vui lòng thử lại sau!";
+            }
+
+            
+
+            echo json_encode([
+                'check' => $check,
+                'msg' => $msg,
+            ]);
+        } else {
+            Response::redirect("");
+            Session::setFlashData('msg',"Truy cập không hợp lệ!");
+        }
+    }
+
+    public function send(){
+        if (isPost()) {
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $message = $_POST['message'];
+            $check = false;
+            $msg = "";
+
+            $dataInsert = [
+                'name'=>$name,
+                'email'=>$email,
+                'message'=>$message,
+                'status'=>1,
+                'note'=>'Chưa xử lý',
+                'create_at'=>date('Y-m-d H:i:s'),
+            ];
+
+            $status = $this->__model->addTableData('contacts',$dataInsert);
+            if($status){
+                $msg = "Gửi lời nhắn thành công";
+            }else{
+                $msg = "Hệ thống lỗi vui lòng thử lại";
+            }
+
+            
+
+            echo json_encode([
+                'check' => $check,
+                'msg' => $msg,
+            ]);
+        } else {
+            Response::redirect("");
+            Session::setFlashData('msg',"Truy cập không hợp lệ!");
+        }
+    }
+    
+}
