c7d98e173b6b718d55c2587e2941b3af410e805d
Sơn <88760761+ahihison@users.noreply.github.com>
2023-04-03 15:10:19 +0700

    filter size

--- app/views/blocks/shop.php
+++ app/views/blocks/shop.php
@@ -1,5 +1,87 @@
 <!-- Breadcrumb Section Begin -->
+<style>
 
+.shop__sidebar__price .field{
+    height: 45px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+
+
+}
+.shop__sidebar__price .separator{
+    width: 60px;
+    display: flex;
+   height: 1px;
+    align-items: center;
+    justify-content: center;
+    background-color: #999;
+}
+.field input{
+    width: 100%;
+    height: 100%;
+    outline: none;
+    font-size:16px;
+    text-align:center;
+    /* margin-left:12px; */
+    border-radius:5px;
+    border:1px solid #999;
+    -moz-appearance: textfield;
+}
+input[type="number"]::-webkit-inner-spin-button, input[type="number"]::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+}
+.collapse{
+    margin:12px 0;
+}
+.slider{
+    height: 5px;
+    border-radius:5px;
+    background: #ddd;
+    position: relative;
+
+}
+.slider .progress{
+    height: 5px;
+    left: 25%;
+    right: 25%;
+    position: absolute;
+    border-radius: 5px;
+    background-color: #17A2B8;
+}
+.range-input{
+    position: relative;
+}
+.range-input input{
+    position: absolute;
+    width: 100%;
+    height: 5px;
+    top: -5px;
+    background: none;
+    pointer-events: none;
+    -webkit-appearance: none;
+}
+input[type="range"]::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    height: 17px;
+    width: 17px;
+    pointer-events: auto;
+    border-radius: 50%;
+    background: #17A2B8;
+
+}
+input[type="range"]::-moz-rang-thumb {
+    -moz-appearance: none;
+    height: 17px;
+    width: 17px;
+    border: none;
+    pointer-events: auto;
+    border-radius: 50%;
+    background: #17A2B8;
+
+}
+</style>
 <!-- Breadcrumb Section End -->
 
 <!-- Shop Section Begin -->
@@ -57,16 +139,26 @@
                                 </div>
                                 <div id="collapseThree" class="collapse" data-parent="#accordionExample">
                                     <div class="card-body">
-                                        <div class="shop__sidebar__price">
-                                            <ul>
-                                                <li><a href="#">$0.00 - $50.00</a></li>
-                                                <li><a href="#">$50.00 - $100.00</a></li>
-                                                <li><a href="#">$100.00 - $150.00</a></li>
-                                                <li><a href="#">$150.00 - $200.00</a></li>
-                                                <li><a href="#">$200.00 - $250.00</a></li>
-                                                <li><a href="#">250.00+</a></li>
-                                            </ul>
+                                       <div class="shop__sidebar__price" style="display: flex;
+                                                            width: 100%;  justify-content: center;
+                                                                align-items: center;">
+
+                                            <div class="field">
+
+                                                <input type="number" class="input-min" value="2500">
+                                            </div>
+                                            <div class="separator"></div>
+                                            <div class="field">
+                                                <input type="number" class="input-max" value="7500">
+                                            </div>
                                         </div>
+                                    </div>
+                                      <div class="slider">
+                                        <div class="progress"></div>
+                                    </div>
+                                    <div class="range-input">
+                                        <input type="range" class="range-min" min="0" max="10000" value="2500" step ="100"  >
+                                        <input type="range" class="range-max" min="0" max="10000" value="7500" step ="100"  >
                                     </div>
                                 </div>
                             </div>
@@ -178,3 +270,75 @@
     </div>
 </section>
 <!-- Shop Section End -->
+<!-- Shop Section End -->
+<script>
+    const rangeInput = document.querySelectorAll(".range-input input");
+    let priceInput = document.querySelectorAll(".shop__sidebar__price input");
+    let progress = document.querySelector(".slider .progress")
+    let priceGap = 1000;
+    priceInput.forEach((input) => {
+        input.addEventListener("input", (e) => {
+            let minVal = parseInt(priceInput[0].value);
+            let maxVal = parseInt(priceInput[1].value);
+
+           if((maxVal-minVal>=priceGap)&&maxVal<=10000){
+                if(e.target.className ==="input-min"){
+                    rangeInput[0].value = minVal;
+                    progress.style.left = (minVal/rangeInput[0].max)*100 + "%";
+                }else{
+                    rangeInput[1].value = maxVal;
+                    progress.style.right = 100-(maxVal/rangeInput[1].max)*100 + "%";
+                }
+           }
+
+
+        });
+        input.addEventListener("blur",()=>{
+            let minVal = parseInt(priceInput[0].value);
+            let maxVal = parseInt(priceInput[1].value);
+            let minValue = document.querySelector(".input-min");
+            let maxValue = document.querySelector(".input-max");
+            if(maxVal-minVal<priceGap){
+                maxVal= minVal+priceGap;
+                rangeInput[1].value = maxVal;
+                maxValue.value = maxVal;
+                progress.style.right = 100-(maxVal/rangeInput[1].max)*100 + "%";
+            }
+            if(minVal<0){
+                minVal=0;
+                rangeInput[0].value = minVal;
+                minValue.value = minVal;
+                progress.style.left = (minVal/rangeInput[0].max)*100 + "%";
+            }
+            if(maxVal>10000){
+                maxVal=10000;
+                rangeInput[1].value = maxVal;
+                maxValue.value = maxVal;
+                progress.style.right = 100-(maxVal/rangeInput[1].max)*100 + "%";
+            }
+        })
+    });
+
+    rangeInput.forEach((input) => {
+        input.addEventListener("input", (e) => {
+            let minVal = parseInt(e.target.parentElement.children[0].value);
+            let maxVal = parseInt(e.target.parentElement.children[1].value);
+
+           if(maxVal-minVal <priceGap){
+                if(e.target.className ==="range-min"){
+                    rangeInput[0].value = maxVal-priceGap;
+                }else{
+                    rangeInput[1].value = minVal+priceGap;
+                }
+           }else{
+            priceInput[0].value = minVal;
+            priceInput[1].value = maxVal;
+            progress.style.left = (minVal/rangeInput[0].max)*100 + "%";
+            progress.style.right = 100-(maxVal/rangeInput[1].max)*100 + "%";
+           }
+
+
+
+        });
+    });
+</script>
